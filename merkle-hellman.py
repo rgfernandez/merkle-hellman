2b37c6b4e458b42abf6c46e9179e93f7d57251a6
Randy Joseph Fernandez <rgfernandez@ieee.org>
2018-10-14 12:43:04 +0800

    finished code

--- merkle-hellman.py
+++ merkle-hellman.py
@@ -16,6 +16,71 @@ def issuperincreasing(sequence):
 def ismodulusvalid(modulus, sequence):
 	return modulus > (2*sequence[-1])
 
+def applyfunction(b, seq):
+	return sum([int(item)*b[elem] for elem,item in enumerate(seq)])
+
+def applyknapsack(num, seq):
+	res = ''
+
+	for elem in seq[::-1]:
+		if num >= elem:
+			res += '1'
+			num = num - elem
+		else:
+			res += '0'
+
+	res = res[::-1]
+
+	return res
+
+def subdivide(seq, n):
+	blocks = []
+	i = 0
+	while 1:
+		if i*n >= len(seq):
+			break
+		blocks.append(seq[(i*n):(i*n+n)])
+		i += 1
+
+	return blocks
+
+def encrypt(a, m, w, message):
+	b = [w*elem%m for elem in a]
+
+	n = len(b)
+	seq = ''
+	for item in message:
+		equiv = letters.find(item)		# get decimal equivalent
+		equiv = bin(equiv)[2:]			# get binary equivalent
+		equiv = equiv.zfill(5)			# prepend with zeros
+		seq += equiv
+
+	while (len(seq) % n != 0):
+		seq += '10111'				# append 'X'
+
+	blocks = subdivide(seq, n)
+
+	blocks = [applyfunction(b,elem) for elem in blocks]
+
+	return tuple(blocks)
+
+def decrypt(a, m, w, message):
+	r, s, w_inv = extended_gcd(m, w)		# get inverse of w
+	v = [elem*w_inv%m for elem in message]		# decrypt message
+
+	v = [applyknapsack(elem, a) for elem in v]
+
+	res = ''.join(v)
+
+	blocks = subdivide(res, 5)
+
+	blocks = [int(elem,2) for elem in blocks]	# convert to decimal
+
+	blocks = [letters[elem] for elem in blocks]
+	blocks = ''.join(blocks)
+
+	return blocks
+
 def main():
 	resp = input('[E]ncrypt or [D]ecrypt? ').upper()
 	if resp not in ('E', 'D'):
@@ -33,12 +98,19 @@ def main():
 		return
 
 	w = int(input('Type private key w: '))
-	print(gcd(m,w))
 	if gcd(m, w) > 1:
 		print('Private key is NOT relatively prime with modulus. Please run code again.')
 		return
 
-	print('Passed all checks')
+	if resp == 'E':
+		message = input('Type message: ').upper()
+	else:
+		message = tuple(int(n.strip().strip('(').strip(')').strip()) for n in input('Type message: ').split(','))
+
+	if resp == 'E':
+		print(encrypt(a, m, w, message))
+	else:
+		print(decrypt(a, m, w, message))
 
 if __name__ == '__main__':
 	try:
